79ba1b4ec2526194a049ba9c6ff8fbb9e70740fd
Aleksandre <amasurashvili@gmail.com>
2020-11-17 19:57:00 +0400

    add index

--- js/app.js
+++ js/app.js
@@ -19,5 +19,19 @@ document.getElementById('btn3').addEventListener('click', function() {
     alert('Your Booking has Confirmed');
 });
 
+var content = document.getElementById("content");
+var button = document.getElementById("read-more");
+
+button.onclick = function() {
+    if(content.className == "open"){
+        //shrink the box
+        content.className = "";
+        button.innerHTML = "Read More"; 
+    } else  {
+        //expand the box
+        content.className = "open"; 
+        button.innerHTML = "Read Less";  
+    }
+};
 
 
